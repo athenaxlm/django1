3e91b6ad09fba99ba7b89bb29a86bc1561cfa79a
kirito <athenaxlm@hotmail.com>
2018-04-16 17:17:20 +0800

    day08

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'df_cart',
     'df_order',
     'df_user',
-    'tinymce'
+    'tinymce',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'dailyfresh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
 
 
 # Create your views here.
-def register(request):
-    return render(request, 'register.html')
+class RegisterView(View):
+    """类视图：处理注册"""
+
+    def get(self, request):
+        """处理GET请求，返回注册页面"""
+        return render(request, 'register.html')
+
+    def post(self, request):
+        """处理POST请求，实现注册逻辑"""
+        # return HttpResponse('这里实现注册逻辑')
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        return HttpResponse('ok')
